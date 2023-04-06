ee447186f296ec44d17236e057fdd66d162be9cc
Mahesh K <mmk@narola.email>
2023-04-06 14:41:58 +0530

    Movie Mania Demo

--- src/app/services/user-name-validation.service.ts
+++ src/app/services/user-name-validation.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+
 import { UserRegistration } from '../models/userRegistration';
 import { UserService } from './user.service';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
 import { UserRegistration } from '../models/userRegistration';
 
 @Injectable({
